36d0497d5bf4f8c8dd26c64a8898175f4bb63091
C.W. Betts <computers57@hotmail.com>
2018-10-01 11:52:56 -0600

    Update to Swift 4.2

--- AttributesFormatter.swift
+++ AttributesFormatter.swift
@@ -59,7 +59,7 @@ public final class AttributesFormatter: Formatter {
 		return nil
 	}
 	
-	override public func attributedString(for obj: Any?, withDefaultAttributes attrs: [NSAttributedStringKey : Any]?) -> NSAttributedString? {
+	override public func attributedString(for obj: Any?, withDefaultAttributes attrs: [NSAttributedString.Key : Any]?) -> NSAttributedString? {
 		if let aStr = string(for: obj) {
 			return NSAttributedString(string: aStr, attributes: attrs)
 		} else {
